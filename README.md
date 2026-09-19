4786d7ce5b3af9b066aaf87e74ae8213b38c5040
cengineer13 <murodild@gmail.com>
2026-09-19 22:10:00 +0500

    BRB: Burak: Initial setup by Marcus

---
+++ README.md
@@ -0,0 +1,2 @@
+# burak_web_project
+This is restaurant project with Backend Express JS 
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("executed");
+
+import moment from 'moment'
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime)
+
+
